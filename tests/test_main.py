733d0888ca7faa5a1c7b654a01cff57331c2e6a9
shinshin86 <beagles1986@gmail.com>
2018-01-04 07:16:08 +0900

    setup unittest

---
+++ tests/test_main.py
@@ -0,0 +1,35 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+    def test_read_json_data(self):
+        pass
+
+
+    def test_write_result_json(self):
+        pass
+
+
+    def test_create_mst_tag_list(self):
+        pass
+
+
+    def test_add_tag_from_link(self):
+        pass
+
+
+    def test_get_tag_words(self):
+        pass
+
+
+    def test_fetch_empty_tag_list(self):
+        pass
+
+
+    def test_fetch_link_page_text(self):
+        pass
+
+
+    def test_get_tag_word_list(self):
+        pass
+
